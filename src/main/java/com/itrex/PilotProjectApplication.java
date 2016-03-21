a08c549fb1bc66491a4d90439f861fc9ddc15788
mromankevich <tnn2kftz>
2016-03-21 16:04:11 +0300

    1) include file paths in properties

--- src/main/java/com/itrex/PilotProjectApplication.java
+++ src/main/java/com/itrex/PilotProjectApplication.java
@@ -4,6 +4,7 @@ import com.itrex.utils.EventsRouterBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,6 +24,7 @@ public class PilotProjectApplication implements CommandLineRunner {
 	}
 
     @Override
+    @Value("${config.path.actualOutput.evenInfo},${config.path.actualOutput.evenByDay},${config.path.expectedOutput.evenInfoExpected},${config.path.expectedOutput.evenByDayExpected}")
     public void run(String... args) throws Exception {
         if (args.length < 4) {
             log.info("Not all arguments are listed");
